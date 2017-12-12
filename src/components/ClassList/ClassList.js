b01c89a62722f239bb02cdfb0c6c30b22a032c9f
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2017-12-12 15:07:43 -0700

    Step 5 complete - got the student data with componentDidMout and axios then setup the list of students

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'react';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  commponentDidMount () {
+    // this.props.match.params.class    matches the identifier with the one in the server
+    return axios.get(`http://localhost:3005/students?class=${ this.props.match.params.class }`).then( response => {
+      this.setState({ students: response.data })
+    }).catch( console.log() )
   }
 
   render() {
+    let students = this.state.students.map( (e, i) => {
+      return <h3 key={i}>{ `${e.first_name} ${e.last_name}` }</h3>
+    })
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{ this.props.match.params.class }</h1>
         <h2>ClassList:</h2>
-        
+        { students }
       </div>
     )
   }
